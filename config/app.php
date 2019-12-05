269c9f5db9c2e0f703870bdd757a2c23ea21149c
elizadg <elizabeth.dominguez@cdmx.gob.mx>
2019-12-04 19:25:13 -0600

    cambio dev

--- config/app.php
+++ config/app.php
@@ -124,11 +124,7 @@ return [
     'cipher' => 'AES-256-CBC',
 
 
-    'debug_blacklist' => [
-            '_COOKIE' => array_keys($_COOKIE),
-            '_SERVER' => array_keys($_SERVER),
-            '_ENV' => array_keys($_ENV),        
-        ],
+    
 
     /*
     |--------------------------------------------------------------------------
