b6d6673b09e3108b24e71f2079701f6c9a7a6cac
elizadg <elizabeth.dominguez@cdmx.gob.mx>
2019-12-10 11:02:24 -0600

    cambios en login

--- resources/views/template/header.blade.php
+++ resources/views/template/header.blade.php
@@ -95,7 +95,7 @@ if (isset(\Auth::user()->permisos)) {
           <div class="container-fluid">
             <div class="navbar-holder d-flex align-items-center justify-content-between">
               <div class="navbar-header"><a id="toggle-btn" href="#" class="menu-btn"><i class="icon-bars"> </i></a><a href="index.html" class="navbar-brand">
-                  <div class="brand-text d-none d-md-inline-block"><span>Gabinete de Seguridad Ciudadana</span><strong class="text-primary">CDMX</strong></div></a></div>
+                  <div class="brand-text d-none d-md-inline-block"><span>CGGSCYPJ</span><strong class="text-primary">CDMX</strong></div></a></div>
               <ul class="nav-menu list-unstyled d-flex flex-md-row align-items-md-center">
                 
                 
